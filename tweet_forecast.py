d7f9f1bef6c4c22bc8c23f80e86603aca316e230
Terence Eden <edent@users.noreply.github.com>
2017-02-27 19:40:58 +0000

    Fix getting the most recent statement

--- tweet_forecast.py
+++ tweet_forecast.py
@@ -10,18 +10,28 @@ from io import open as iopen
 # Get the data from the Met Office
 data = requests.get("https://api.ffc-environment-agency.fgs.metoffice.gov.uk/api/public/statements").json()
 
-# Last element is most recent. Get forecast
-forecast_text = data["statements"][-1]["public_forecast"]["english_forecast"]
+# Get the most recent statment
+most_recent = 0
+for statement in data["statements"]:
+   if statement["id"] > most_recent:
+      most_recent = statement["id"]
+recent_statement = requests.get("https://api.ffc-environment-agency.fgs.metoffice.gov.uk/api/public/statements/"+str(most_recent)).json()
 
-# Get Image
-forecast_image_url = data["statements"][-1]["png_thumbnails_with_days_url"]
+# Get the text
+forecast_text = recent_statement["statement"]["headline"]
+
+if len(forecast_text) > 114:
+   forecast_text = forecast_text[:114] + u"…\n"
+
+# Get the image
+forecast_image_url = recent_statement["statement"]["png_thumbnails_with_days_url"]
 
 # Save Image
 photo_path = 'forecast_image.png'
 image = requests.get(forecast_image_url)
 with iopen(photo_path, 'wb') as file:
    file.write(image.content)
-   
+
 # Replace alpha with white
 png = Image.open(photo_path)
 png.load() # required for png.split()
@@ -38,7 +48,7 @@ access_token_secret = ''
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
+ 
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 
@@ -47,8 +57,10 @@ info_url = 'https://flood-warning-information.service.gov.uk/5-day-flood-risk'
 
 # Send the tweet with photo
 photo_path = 'forecast_image.png'
-status = forecast_text[:114] + u"…\n"  + info_url
-api.update_with_media(photo_path, status=status)
 
-# Delete the photo
+status = forecast_text + "\n" + info_url
+
+#print status
+
+api.update_with_media(photo_path, status=status)
 os.remove(photo_path)
